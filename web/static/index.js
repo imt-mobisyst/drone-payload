e3d10ef5182433699783e50071cb05b169228a6f
Alutulu <arthur.mata24@gmail.com>
2024-09-23 10:55:24 +0200

    draft

--- web/static/index.js
+++ web/static/index.js
@@ -1,41 +1,35 @@
-let timer;
-let seconds = 0;
-let loadingMessage = document.getElementById('loading-message');
+let timers = [null, null];
+let seconds = [0, 0];
 
 function confirmAction(id) {
     let value = document.getElementById(id)
-    console.log(value)
-    if (value.checked) {
-        startTimer();
-        open = true
-    } else {
-        stopTimer();
-        open = false
-    }
     fetch("", {
         method: "POST",
         body: JSON.stringify({
-          open: open,
-          valve_nb: id
+            open: value.checked,
+            valve_nb: id
         }),
         headers: {
-          "Content-type": "application/json; charset=UTF-8"
+            "Content-type": "application/json; charset=UTF-8"
         }
-    }).then( async (resp) => {
-        if(resp.ok){
+    }).then(async (resp) => {
+        if (resp.ok) {
             let data = await resp.json()
             let valve = document.getElementById('valve-' + String(id))
-            if(data[id].is_open){
+            let numTimer = id == "v1" ? 0 : 1
+            if (data[id].is_open) {
+                startTimer(numTimer);
                 valve.style.backgroundColor = "#6bd653"
                 valve.style.boxShadow = "8px 8px #479238"
                 valve.children[1].children[1].textContent = "Opened"
             } else {
+                stopTimer(numTimer);
                 valve.style.backgroundColor = "#f0f0f0"
                 valve.style.boxShadow = "8px 8px #d0d0d0"
                 valve.children[1].children[1].textContent = "Closed"
             }
-
             console.log(data)
+            console.log(`Timer ${numTimer} : ${value}`)
         }
     });
     closeModal();
@@ -51,34 +45,31 @@ function closeModal() {
     document.getElementById('confirmation-modal').style.display = 'none';
 }
 
-function startTimer() {
-    if (!timer) {
-        timer = setInterval(updateTimer, 1000);
-        //fetch('./dist/ouvrir.php');
-        //loadingMessage.style.display = 'flex';
+function startTimer(numTimer) {
+    if (!timer[numTimer]) {
+        timer[numTimer] = setInterval(() => updateTimer(numTimer), 1000);
     }
 }
 
-function stopTimer() {
-    if (timer) {
-        clearInterval(timer);
-        timer = null;
-        //fetch('./dist/fermer.php');
-        //loadingMessage.style.display = 'none';
+function stopTimer(numTimer) {
+    if (timer[numTimer]) {
+        clearInterval(timer[numTimer]);
+        timer[numTimer] = null;
     }
 }
 
-function resetTimer() {
-    seconds = 0;
-    updateTimerDisplay();
+function resetTimer(numTimer) {
+    seconds[numTimer] = 0;
+    updateTimerDisplay(numTimer);
 }
 
-function updateTimer() {
+function updateTimer(numTimer) {
     seconds++;
-    updateTimerDisplay();
+    updateTimerDisplay(numTimer);
 }
 
-function updateTimerDisplay() {
-    document.getElementById('timer').textContent = seconds > 9 ? seconds : "0" + seconds;
+function updateTimerDisplay(numTimer) {
+    let updateTimer = document.getElementById(numTimer == 0 ? 'timer1' : 'timer2')
+    updateTimer.textContent = seconds[numTimer] > 9 ? seconds[numTimer] : "0" + seconds[numTimer];
 }
 
